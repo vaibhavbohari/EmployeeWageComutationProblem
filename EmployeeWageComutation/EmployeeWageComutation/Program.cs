f0ebccca3cf816871ec25880cba482a1d8028878
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-19 18:47:06 +0530

    CheckEmployee

--- EmployeeWageComutation/EmployeeWageComutation/Program.cs
+++ EmployeeWageComutation/EmployeeWageComutation/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to Employee Wage Computation");
+
+EmployeeWage empWage = new EmployeeWage();
+empWage.StartWageCal();
+
