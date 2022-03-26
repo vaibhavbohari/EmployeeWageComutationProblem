ce04c72518378998d40ffcf76152512147b3872f
vaibhavbohari <vaibhavbohari@gmail.com>
2022-03-26 20:19:01 +0530

    Added Multi Comanies Files

--- EmployeeWageComutation/EmployeeWageComutation/EmployeeWage.cs
+++ EmployeeWageComutation/EmployeeWageComutation/EmployeeWage.cs
@@ -7,15 +7,11 @@ namespace EmployeeWageComputation
 {
     class EmployeeWage
     {
-        public const int IS_PART_TIME = 1, 
-            IS_FULL_TIME = 2,
-            EMP_RATE_PER_HOUR = 20,
-            NUM_OF_WORKING_DAYS = 20, 
-            MAX_HRS_IN_MONTH = 100;
-        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0, totalEmpWage;
-        public void CalculateEmpWage()
+        public const int IS_PART_TIME = 1, IS_FULL_TIME = 2;
+        public static int monthlyEmployeeWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0, totalEmpWage;
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -35,9 +31,9 @@ namespace EmployeeWageComputation
                 totalEmpHrs += empHrs;
                 Console.WriteLine("Day#:" + totalWorkingDays + " Employee Hours : " + empHrs);
             }
-            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Employee Wage : " + totalEmpWage);
-
+            totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine(company + " Total Employee Wage : " + totalEmpWage);
+            return totalEmpWage;
         }
     }
 }
--- EmployeeWageComutation/EmployeeWageComutation/Program.cs
+++ EmployeeWageComutation/EmployeeWageComutation/Program.cs
@@ -6,8 +6,10 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            EmployeeWage  wagecomputation = new EmployeeWage ();
-            wagecomputation.CalculateEmpWage();
+            Console.WriteLine("Welcome to the Employee Wage Computation Problem");
+            EmployeeWage.monthlyEmployeeWage("PVP", 20, 20, 100);
+            EmployeeWage.monthlyEmployeeWage("DMART", 30, 24, 140);
         }
     }
 }
+
